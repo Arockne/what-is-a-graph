7e4a2e045e6955f2b10635327eaf7b6d8007b369
Daniel Calvin <arockne2003@yahoo.com>
2022-04-04 10:58:03 -0700

    commit tree 7890670fa472f62e4933db3b09ea1aae524038ca

--- javascript/graph.js
+++ javascript/graph.js
@@ -1,15 +1,39 @@
 class Graph {
   constructor(paths) {
-
+    this.graph = {};
+    for (const path of paths) {
+      for (let i = 0; i < path.length; i += 1) {
+        const currentNode = path[i]
+        if (!this.graph[currentNode]) {
+          this.graph[currentNode] = new Set();
+        }
+        if (path[i-1]) {
+          this.graph[currentNode].add(path[i-1])
+        }
+        if (path[i+1]) {
+          this.graph[currentNode].add(path[i+1])
+        }
+      }
+    }
   }
 
   isAdjacent(vertexA, vertexB) {
-
+    return this.graph[vertexA].has(vertexB)
   }
 
   // array is an adjacency list
-  addVertex(vertex, array) {
-
+  addVertex(vertex, array) { 
+    for (const node of array) {
+      if (!this.graph[vertex]) {
+        this.graph[vertex] = new Set();
+      }
+      if (!this.graph[node]) {
+        this.graph[node] = new Set()
+      }
+      
+      this.graph[node].add(vertex)
+      this.graph[vertex].add(node)
+    }
   }
 }
 
